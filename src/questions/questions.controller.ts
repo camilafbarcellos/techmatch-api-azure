b3c5fd31a6faaf9cde3d628c55b2dc9e2c6be761
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-01-31 18:04:21 -0300

    feat: add exceptions handling

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,49 +1,70 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import * as questionsService from './questions.service';
 
-async function getAll(req: Request, res: Response, next: NextFunction) {
-    try {
-      res.json(await questionsService.findAll());
-    } catch (err: any) {
-      console.error(`Error while getting the question:s`, err.message);
-      next(err);
+async function getAll(req: Request, res: Response) {
+  try {
+    const questions = await questionsService.findAll();
+    res.status(200).json(questions);
+  } catch (error) {
+    console.error('Error while questions:', error);
+    res.status(500).send('Internal Server Error');
+  }
+}
+
+async function get(req: Request, res: Response) {
+  try {
+    const questionId = req.params.id;
+    const question = await questionsService.findOne(questionId);
+    if (!question) {
+      res.status(404).json({ message: 'Question not found' });
+    } else {
+      res.status(200).json(question);
     }
-  }
-
-async function get(req: Request, res: Response, next: NextFunction) {
-  try {
-    res.json(await questionsService.findOne(req.params.id));
-  } catch (err: any) {
-    console.error(`Error while getting the question:`, err.message);
-    next(err);
+  } catch (error) {
+    console.error('Error while getting the question:', error);
+    res.status(500).send('Internal Server Error');
   }
 }
 
-async function create(req: Request, res: Response, next: NextFunction) {
+async function create(req: Request, res: Response) {
   try {
-    res.json(await questionsService.create(req.body));
-    
-  } catch (err: any) {
-    console.error(`Error while creating the question`, err.message);
-    next(err);
+    const newQuestion = req.body;
+    const createdQuestion = await questionsService.create(newQuestion);
+    res.status(201).json(createdQuestion);
+  } catch (error) {
+    console.error('Error while creating the question:', error);
+    res.status(500).send('Internal Server Error');
   }
 }
 
-async function update(req: Request, res: Response, next: NextFunction) {
+async function update(req: Request, res: Response) {
   try {
-    res.json(await questionsService.update(req.params.id, req.body));
-  } catch (err: any) {
-    console.error(`Error while updating the question`, err.message);
-    next(err);
+    const questionId = req.params.id;
+    const updatedQuestionData = req.body;
+    const updatedQuestion = await questionsService.update(questionId, updatedQuestionData);
+    if (!updatedQuestion) {
+      res.status(404).json({ message: 'Question not found' });
+    } else {
+      res.status(200).json(updatedQuestion);
+    }
+  } catch (error) {
+    console.error('Error while updating the question:', error);
+    res.status(500).send('Internal Server Error');
   }
 }
 
-async function remove(req: Request, res: Response, next: NextFunction) {
+async function remove(req: Request, res: Response) {
   try {
-    res.json(await questionsService.remove(req.params.id));
-  } catch (err: any) {
-    console.error(`Error while deleting the question`, err.message);
-    next(err);
+    const questionId = req.params.id;
+    const deletedQuestion = await questionsService.remove(questionId);
+    if (!deletedQuestion) {
+      res.status(404).json({ message: 'Question not found' });
+    } else {
+      res.status(200).json(deletedQuestion);
+    }
+  } catch (error) {
+    console.error('Error removing question:', error);
+    res.status(500).send('Internal Server Error');
   }
 }
 
