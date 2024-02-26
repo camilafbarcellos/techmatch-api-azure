67479fce83095119cf6ceacbeae387f35df31251
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-02-26 19:34:42 -0300

    feat: add data caching

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,7 +1,26 @@
+import NodeCache from 'node-cache';
 import Questions, { IQuestion } from './question.model';
 
+// Cache instance
+const cache = new NodeCache();
+
+// Function to manually invalidate the cache for when adding, updating or removing
+function invalidateCache() {
+  cache.del('questions');
+}
+
+// Fetch and store questions in cache
+async function findAllAndCache(): Promise<IQuestion[]> {
+  const questions = await Questions.find();
+  cache.set('questions', questions);
+  return questions;
+}
+
 async function findAll(): Promise<IQuestion[]> {
-  return Questions.find();
+  // Check if data is already cached
+  const cachedData = cache.get<IQuestion[]>('questions');
+  // If not cached, search the database and cache it
+  return cachedData || findAllAndCache();
 }
 
 async function findOne(id: string): Promise<IQuestion | null> {
@@ -9,14 +28,17 @@ async function findOne(id: string): Promise<IQuestion | null> {
 }
 
 async function create(data: IQuestion) {
+  invalidateCache();
   return new Questions(data).save();
 }
 
 async function update(id: string, data: IQuestion) {
+  invalidateCache();
   return Questions.findByIdAndUpdate(id, data, { new: true });
 }
 
 async function remove(id: string) {
+  invalidateCache();
   return Questions.findByIdAndDelete(id);
 }
 
