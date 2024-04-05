2756e21df8e83ba434dd977d82ce94739501a29c
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-04-04 21:47:04 -0300

    feat: add questions category enum

---
+++ src/utils/questionCategory.ts
@@ -0,0 +1,7 @@
+export enum QuestionCategory {
+    DESENVOLVIMENTO = 'Desenvolvimento',
+    INFRAESTRUTURA_TI = 'Infraestrutura de TI',
+    CIBERSEGURANCA = 'Cibersegurança',
+    CIENCIA_DE_DADOS = 'Ciência de Dados',
+    DESAFIOS = 'Desafios'
+}
