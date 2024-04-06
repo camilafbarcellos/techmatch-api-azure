aec47e0f576cccc35e4da6077c0dd1674f1a970e
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-04-06 01:13:26 -0300

    refactor: remove unused code

--- src/__tests__/features/auth.test.ts
+++ src/__tests__/features/auth.test.ts
@@ -1,34 +1,24 @@
-import mongoose from 'mongoose';
 import request from 'supertest';
-import dotenv from 'dotenv';
 import app from '../../app';
 import { adminInput, invalidInput } from '../fixtures/auth.fixture';
 
-dotenv.config();
-
-/* Connecting to the database before the tests */
-beforeAll(async () => {
-    await mongoose.connect(process.env.ATLAS_URI as string);
-});
-
-/* Closing database connection after the tests */
-afterAll(async () => {
-    await mongoose.connection.close();
-});
-
 describe('POST /auth', () => {
-    it('should authenticate and return a JWT token', async () => {
-        const res = await request(app)
-            .post('/auth')
-            .send(adminInput);
-        expect(res.statusCode).toBe(200);
-        expect(res.body).toEqual({ accessToken: expect.any(String) });
+    describe('given credentials are valid', () => {
+        it('should return a JWT token (200)', async () => {
+            const res = await request(app)
+                .post('/auth')
+                .send(adminInput);
+            expect(res.statusCode).toBe(200);
+            expect(res.body).toEqual({ accessToken: expect.any(String) });
+        });
     });
 
-    it('should fail authentication and return an unauthorized message', async () => {
-        const res = await request(app)
-            .post('/auth')
-            .send(invalidInput);
-        expect(res.statusCode).toBe(401);
+    describe('given credentials are invalid', () => {
+        it('should return an unauthorized status (401)', async () => {
+            const res = await request(app)
+                .post('/auth')
+                .send(invalidInput);
+            expect(res.statusCode).toBe(401);
+        });
     });
 });
