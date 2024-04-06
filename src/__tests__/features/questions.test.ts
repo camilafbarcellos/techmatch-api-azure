81a79f22b1ac4b37181141a76a3c20fc5eeb088e
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2024-04-06 01:14:09 -0300

    feat: use mongodb memory server for jest tests

--- src/__tests__/features/questions.test.ts
+++ src/__tests__/features/questions.test.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { ObjectId } from 'mongodb';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import request from 'supertest';
 import dotenv from 'dotenv';
 import app from '../../app';
@@ -13,6 +14,7 @@ import {
 
 dotenv.config();
 
+let mongoServer: MongoMemoryServer;
 let token: string;
 let questionId: ObjectId;
 const invalidQuestionId: ObjectId = new mongoose.Types.ObjectId();
@@ -21,139 +23,172 @@ const invalidQuestionId: ObjectId = new mongoose.Types.ObjectId();
 beforeAll(async () => {
     const res = await request(app).post('/auth').send(adminInput);
     token = res.body.accessToken;
-    await mongoose.connect(process.env.ATLAS_URI as string);
+
+    mongoServer = await MongoMemoryServer.create();
+    await mongoose.connect(mongoServer.getUri());
 });
 
 /* Closing database connection after the tests */
 afterAll(async () => {
     await mongoose.connection.close();
+    await mongoServer.stop();
+});
+
+describe('POST /questions', () => {
+    describe('given question is valid', () => {
+        it('should create a question and return it (201)', async () => {
+            const res = await request(app)
+                .post('/questions')
+                .send(questionInput)
+                .set('Authorization', `Bearer ${token}`);
+            expect(res.statusCode).toBe(201);
+            expect(res.body).toEqual({ ...questionPayload, _id: expect.any(String) });
+            questionId = res.body._id;
+        });
+    });
+
+    describe('given question is invalid', () => {
+        it('should return a bad request status (400)', async () => {
+            const res = await request(app)
+                .post('/questions')
+                .send(invalidQuestionInput)
+                .set('Authorization', `Bearer ${token}`);
+            expect(res.statusCode).toBe(400);
+        });
+    });
+
+    describe('given JWT token is invalid', () => {
+        it('should return an unauthorized status (401)', async () => {
+            const res = await request(app)
+                .post('/questions')
+                .send(questionInput)
+                .set('Authorization', `Bearer invalidToken`);
+            expect(res.statusCode).toBe(401);
+        });
+    });
+
+    describe('no given JWT token', () => {
+        it('should return a forbidden status (403)', async () => {
+            const res = await request(app)
+                .post('/questions')
+                .send(questionInput);
+            expect(res.statusCode).toBe(403);
+        });
+    });
 });
 
 describe('GET /questions', () => {
-    it('should return all questions', async () => {
+    it('should return all questions (200)', async () => {
         const res = await request(app).get('/questions');
         expect(res.statusCode).toBe(200);
         expect(res.body.length).toBeGreaterThan(0);
     });
 });
 
-describe('POST /questions', () => {
-    it('should create a question and return it', async () => {
-        const res = await request(app)
-            .post('/questions')
-            .send(questionInput)
-            .set('Authorization', `Bearer ${token}`);
-        expect(res.statusCode).toBe(201);
-        expect(res.body).toEqual({ ...questionPayload, _id: expect.any(String) });
-        questionId = res.body._id;
-    });
-
-    it('should fail operation and return a bad request message', async () => {
-        const res = await request(app)
-            .post('/questions')
-            .send(invalidQuestionInput)
-            .set('Authorization', `Bearer ${token}`);
-        expect(res.statusCode).toBe(400);
-    });
-
-    it('should fail operation and return an unauthorized message', async () => {
-        const res = await request(app)
-            .post('/questions')
-            .send(questionInput)
-            .set('Authorization', `Bearer invalidToken`);
-        expect(res.statusCode).toBe(401);
-    });
-
-    it('should fail operation and return a forbidden message', async () => {
-        const res = await request(app)
-            .post('/questions')
-            .send(questionInput);
-        expect(res.statusCode).toBe(403);
-    });
-});
-
 describe('GET /questions/:id', () => {
-    it('should return a question', async () => {
-        const res = await request(app)
-            .get(`/questions/${questionId}`);
-        expect(res.statusCode).toBe(200);
-        expect(res.body).toEqual({ ...questionPayload, _id: questionId });
+    describe('given question ID is valid', () => {
+        it('should return a question (200)', async () => {
+            const res = await request(app)
+                .get(`/questions/${questionId}`);
+            expect(res.statusCode).toBe(200);
+            expect(res.body).toEqual({ ...questionPayload, _id: questionId });
+        });
     });
 
-    it('should fail operation and return a not found message', async () => {
-        const res = await request(app)
-            .get(`/questions/${invalidQuestionId}`);
-        expect(res.statusCode).toBe(404);
+    describe('given question ID is invalid', () => {
+        it('should return a not found status (404)', async () => {
+            const res = await request(app)
+                .get(`/questions/${invalidQuestionId}`);
+            expect(res.statusCode).toBe(404);
+        });
     });
 });
 
 describe('PUT /questions/:id', () => {
-    it('should update a question and return it', async () => {
-        const res = await request(app)
-            .put(`/questions/${questionId}`)
-            .send(updateQuestionInput)
-            .set('Authorization', `Bearer ${token}`);
-        expect(res.statusCode).toBe(200);
-        expect(res.body).toEqual({ ...updateQuestionPayload, _id: questionId });
+    describe('given question ID and data are valid', () => {
+        it('should update a question and return it (200)', async () => {
+            const res = await request(app)
+                .put(`/questions/${questionId}`)
+                .send(updateQuestionInput)
+                .set('Authorization', `Bearer ${token}`);
+            expect(res.statusCode).toBe(200);
+            expect(res.body).toEqual({ ...updateQuestionPayload, _id: questionId });
+        });
     });
 
-    it('should fail operation and return a not found message', async () => {
-        const res = await request(app)
-            .put(`/questions/${invalidQuestionId}`)
-            .send(updateQuestionInput)
-            .set('Authorization', `Bearer ${token}`);
-        expect(res.statusCode).toBe(404);
+    describe('given question ID is invalid', () => {
+        it('should return a not found status (404)', async () => {
+            const res = await request(app)
+                .put(`/questions/${invalidQuestionId}`)
+                .send(updateQuestionInput)
+                .set('Authorization', `Bearer ${token}`);
+            expect(res.statusCode).toBe(404);
+        });
     });
 
-    it('should fail operation and return a bad request message', async () => {
-        const res = await request(app)
-            .put(`/questions/${questionId}`)
-            .send(updateInvalidCategoryInput)
-            .set('Authorization', `Bearer ${token}`);
-        expect(res.statusCode).toBe(400);
+    describe('given question is invalid', () => {
+        it('should fail operation and return a bad request status (400)', async () => {
+            const res = await request(app)
+                .put(`/questions/${questionId}`)
+                .send(updateInvalidCategoryInput)
+                .set('Authorization', `Bearer ${token}`);
+            expect(res.statusCode).toBe(400);
+        });
     });
 
-    it('should fail operation and return an unauthorized message', async () => {
-        const res = await request(app)
-            .put(`/questions/${questionId}`)
-            .send(updateQuestionInput)
-            .set('Authorization', `Bearer invalidToken`);
-        expect(res.statusCode).toBe(401);
+    describe('given JWT token is invalid', () => {
+        it('should return an unauthorized status (401)', async () => {
+            const res = await request(app)
+                .put(`/questions/${questionId}`)
+                .send(updateQuestionInput)
+                .set('Authorization', `Bearer invalidToken`);
+            expect(res.statusCode).toBe(401);
+        });
     });
 
-    it('should fail operation and return a forbidden message', async () => {
-        const res = await request(app)
-            .put(`/questions/${questionId}`)
-            .send(updateQuestionInput)
-        expect(res.statusCode).toBe(403);
+    describe('no given JWT token', () => {
+        it('should return a forbidden status (403)', async () => {
+            const res = await request(app)
+                .put(`/questions/${questionId}`)
+                .send(updateQuestionInput)
+            expect(res.statusCode).toBe(403);
+        });
     });
 });
 
 describe('DELETE /questions/:id', () => {
-    it('should delete a question', async () => {
-        const res = await request(app)
-            .delete(`/questions/${questionId}`)
-            .set('Authorization', `Bearer ${token}`);
-        expect(res.statusCode).toBe(204);
+    describe('given question ID is valid', () => {
+        it('should delete a question (204)', async () => {
+            const res = await request(app)
+                .delete(`/questions/${questionId}`)
+                .set('Authorization', `Bearer ${token}`);
+            expect(res.statusCode).toBe(204);
+        });
     });
 
-    it('should fail operation and return a not found message', async () => {
-        const res = await request(app)
-            .delete(`/questions/${invalidQuestionId}`)
-            .set('Authorization', `Bearer ${token}`);
-        expect(res.statusCode).toBe(404);
+    describe('given question ID is invalid', () => {
+        it('should return a not found status (404)', async () => {
+            const res = await request(app)
+                .delete(`/questions/${invalidQuestionId}`)
+                .set('Authorization', `Bearer ${token}`);
+            expect(res.statusCode).toBe(404);
+        });
     });
 
-    it('should fail operation and return an unauthorized message', async () => {
-        const res = await request(app)
-            .delete(`/questions/${questionId}`)
-            .set('Authorization', `Bearer invalidToken`);
-        expect(res.statusCode).toBe(401);
+    describe('given JWT token is invalid', () => {
+        it('should return an unauthorized status (401)', async () => {
+            const res = await request(app)
+                .delete(`/questions/${questionId}`)
+                .set('Authorization', `Bearer invalidToken`);
+            expect(res.statusCode).toBe(401);
+        });
     });
 
-    it('should fail operation and return a forbidden message', async () => {
-        const res = await request(app)
-            .delete(`/questions/${questionId}`)
-        expect(res.statusCode).toBe(403);
+    describe('no given JWT token', () => {
+        it('should return a forbidden status (403)', async () => {
+            const res = await request(app)
+                .delete(`/questions/${questionId}`)
+            expect(res.statusCode).toBe(403);
+        });
     });
 });
